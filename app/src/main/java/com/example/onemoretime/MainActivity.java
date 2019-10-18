8e22b25a91f8deb5bf0399a048e7db972e6394e2
kacfal <kacfal181195@gmail.com>
2019-10-18 21:10:02 +0200

    Add reading taf by nfc

--- app/src/main/java/com/example/onemoretime/MainActivity.java
+++ app/src/main/java/com/example/onemoretime/MainActivity.java
@@ -3,15 +3,12 @@ package com.example.onemoretime;
 import android.app.Activity;
 import android.content.Intent;
 import android.nfc.NfcAdapter;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
 
-
 public class MainActivity extends Activity {
-
-    public static final String TAG = "NfcDemo";
-
     private TextView mTextView;
     private NfcAdapter mNfcAdapter;
 
@@ -40,7 +37,68 @@ public class MainActivity extends Activity {
         handleIntent(getIntent());
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        /**
+         * It's important, that the activity is in the foreground (resumed). Otherwise
+         * an IllegalStateException is thrown.
+         */
+    }
+
+    @Override
+    protected void onPause() {
+        /**
+         * Call this before onPause, otherwise an IllegalArgumentException is thrown as well.
+         */
+        super.onPause();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        /**
+         * This method gets called, when a new Intent gets associated with the current activity instance.
+         * Instead of creating a new activity, onNewIntent will be called. For more information have a look
+         * at the documentation.
+         *
+         * In our case this method gets called, when the user attaches a Tag to the device.
+         */
+        handleIntent(intent);
+    }
+
+
     private void handleIntent(Intent intent) {
-        // TODO: handle Intent
+        String action = intent.getAction();
+        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)) {
+
+
+
+            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            if(tag == null){
+                Toast.makeText(this,"Try again.", Toast.LENGTH_SHORT).show();
+                mTextView.setText("tag == null");
+            }else{
+                String tagInfo = tag.toString() + "\n";
+
+                tagInfo += "\nTag Id: \n";
+                byte[] tagId = tag.getId();
+                tagInfo += "length = " + tagId.length +"\n";
+                for(int i=0; i<tagId.length; i++){
+                    tagInfo += Integer.toHexString(tagId[i] & 0xFF) + " ";
+                }
+                tagInfo += "\n";
+
+                String[] techList = tag.getTechList();
+                tagInfo += "\nTech List\n";
+                tagInfo += "length = " + techList.length +"\n";
+                for(int i=0; i<techList.length; i++){
+                    tagInfo += techList[i] + "\n ";
+                }
+
+                mTextView.setText(tagInfo);
+                Toast.makeText(this, "Student Card was registered.",
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
